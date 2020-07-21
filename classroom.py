d7f2aaf7898f951f00a107039d7270afceec0df7
Chyld Medford <chyld.medford@gmail.com>
2020-07-21 14:23:53 -0700

    finished

---
+++ classroom.py
@@ -0,0 +1,29 @@
+from student import Student
+
+
+class Classroom:
+    def __init__(self, name):
+        self.name = name
+        self.students = {}
+
+    def read_students(self, filename):
+        with open(filename) as f:
+            for line in f:
+                name, grade = line.strip().split(', ')
+                grade = float(grade)
+                if name in self.students:
+                    s = self.students[name]
+                    s.grades.append(grade)
+                else:
+                    s = Student(name)
+                    s.grades.append(grade)
+                    self.students[name] = s
+
+    def calculate_averages(self):
+        for name in self.students:
+            s = self.students[name]
+            s.update()
+
+    def best(self):
+        n = max(self.students, key=lambda n: self.students[n].average)
+        return self.students[n]
---
+++ main.py
@@ -0,0 +1,10 @@
+from classroom import Classroom
+
+physics = Classroom(name='physics')
+physics.read_students('grades.csv')
+physics.calculate_averages()
+for name in physics.students:
+    s = physics.students[name]
+    print(s)
+best = physics.best()
+print('best:', best)
